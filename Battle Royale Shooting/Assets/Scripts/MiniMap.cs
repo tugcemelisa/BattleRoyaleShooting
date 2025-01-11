5de25b3aed9dc5145246e7adfb5eb8203fd0e4d4
Tugce Melisa <tugcehaspolat@gmail.com>
2025-01-11 15:10:38 +0300

    Update: Minimap, Canvas, Enemy, Scenes

---
+++ Battle Royale Shooting/Assets/Scripts/MiniMap.cs
@@ -0,0 +1,41 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private float scrollSpeed = 1f;
+    [SerializeField] private float minValue = 10f;
+    [SerializeField] private float maxValue = 60f;
+    private float currentValue;
+    void Start()
+    {
+        if (!photonView.IsMine)
+        {
+            gameObject.SetActive(false);
+        }
+        currentValue = 20f;
+    }
+
+    private void Update()
+    {
+        
+        float scrollDelta = Input.GetAxis("Mouse ScrollWheel");
+        // print(scrollDelta);
+
+        
+        if (scrollDelta > 0)
+        {
+            currentValue += scrollSpeed;
+        }
+        else if (scrollDelta < 0)
+        {
+            currentValue -= scrollSpeed;
+        }
+
+       
+        currentValue = Mathf.Clamp(currentValue, minValue, maxValue);
+        gameObject.GetComponent<Camera>().orthographicSize = currentValue;
+    }
+}
